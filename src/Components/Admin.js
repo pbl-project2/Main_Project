4f10cc17f5d1ff72336ef040598610716c7b6c25
cipher <fynhacker@gmail.com>
2022-03-05 16:13:04 +0530

    admin side css changed

--- src/Components/Admin.js
+++ src/Components/Admin.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { db } from "../firebase/firebase";
 import "../Styling/Admin.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 function Admin({ user, handleDelete, income }) {
   const [food, setFood] = useState([]);
@@ -8,7 +9,8 @@ function Admin({ user, handleDelete, income }) {
   // const [completedArr1, setCompletedArr1] = useState([]);
   useEffect(() => {
     db.collection("users")
-      .doc(`${user.id}`).collection("food")
+      .doc(`${user.id}`)
+      .collection("food")
       .onSnapshot((snapshot) => {
         let foodArr = [];
         snapshot.forEach((doc) => {
@@ -18,36 +20,37 @@ function Admin({ user, handleDelete, income }) {
         console.log("FOOD ARR: ", foodArr);
       });
   }, [db]);
-    // const handleComplete = async (id) => {
-    //   await db.collection("users").doc(`${user.id}`).collection("food").doc(`${id}`).update({
-    //     completed: true,
-    //   });
-    //   setCompleted(true);
-    //   await db.collection("users").where("completed", "==", false).get().then(function(querySnapshot) {
-    //     let completedArr = [];
-    //     querySnapshot.forEach(function(doc) {
-    //       completedArr.push({ ...doc.data(), id: doc.id });
-    //     });
-    //     setCompletedArr1(completedArr);
-    //     console.log("COMPLETED ARR: ", completedArr1);
-    //   });
-    // }
-    const handleComplete = async (id) => {
-      await db.collection("users").doc(`${user.id}`).update({
-        completed: true,
-      });
-    }
+  // const handleComplete = async (id) => {
+  //   await db.collection("users").doc(`${user.id}`).collection("food").doc(`${id}`).update({
+  //     completed: true,
+  //   });
+  //   setCompleted(true);
+  //   await db.collection("users").where("completed", "==", false).get().then(function(querySnapshot) {
+  //     let completedArr = [];
+  //     querySnapshot.forEach(function(doc) {
+  //       completedArr.push({ ...doc.data(), id: doc.id });
+  //     });
+  //     setCompletedArr1(completedArr);
+  //     console.log("COMPLETED ARR: ", completedArr1);
+  //   });
+  // }
+  const handleComplete = async (id) => {
+    await db.collection("users").doc(`${user.id}`).update({
+      completed: true,
+    });
+  };
 
   return (
     <>
-      <div className="container">
-        <div className="adminuser">
-          <h1>{user.token}</h1>
-          <p>{user.name}</p>
+      <div className="order">
+        {/* <div className="admin-user">
+          
+          <h1></h1>
+          <p></p>
           <p>{user.mobile}</p>
           {food.map((item) => (
             <div className="food-items">
-              <p>{item.name}</p>
+              <p className="admin-food-name">{item.name}</p>
               <p> x {item.quantity}</p>
               <p>₹{item.price}</p>
             </div>
@@ -56,6 +59,28 @@ function Admin({ user, handleDelete, income }) {
           <button className="delete-btn" onClick={() => handleDelete(user.id)}>
             Delete
           </button>
+        </div> */}
+        <div class="card">
+          <div class="card-header order-title">Featured</div>
+          <div class="card-body">
+            <h2 class="card-token">{user.token}</h2>
+            <h5 class="card-title">{user.name}</h5>
+            {food.map((item) => (
+              <div className="food-items">
+                <p className="admin-food-name">{item.name}</p>
+                <p className="food-quantity"> x{item.quantity}</p>
+                <p className="food-price">₹{item.price}</p>
+              </div>
+            ))}
+            <h5>Total: ₹{user.total}</h5>
+            <button
+              href="#"
+              class="btn btn-primary complete"
+              onClick={() => handleDelete(user.id)}
+            >
+              Complete
+            </button>
+          </div>
         </div>
       </div>
     </>
