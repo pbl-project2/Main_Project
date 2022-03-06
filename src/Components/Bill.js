22e34acd4f28aab02b5b983b0b5522fab2537818
cipher <fynhacker@gmail.com>
2022-03-06 11:52:31 +0530

    rating stars added

--- src/Components/Bill.js
+++ src/Components/Bill.js
@@ -89,7 +89,7 @@ function Bill() {
     doc.text("Thank you for using our system", 150, 620);
     doc.save(`bill/${token}.pdf`);
   };
-  
+
   return (
     <>
       <div className="admin-nav">
@@ -121,9 +121,35 @@ function Bill() {
           ))}
           <h1 className="total">Total: ₹{total}</h1>
           <p className="messege">**PAY AT THE CANTEEN**</p>
-          <button className="login-btn" onClick={generatePdf}>
+          <button className="login-btn pdf-btn" onClick={generatePdf}>
             Generate PDF
           </button>
+        </div>
+        <div className="rating-div col">
+          <div className="rate-us">
+            <div className="question-div">
+              <h3>Did You Like Our Site?</h3>
+              <h5>Wanna rate it?</h5>
+            </div>
+
+            <div class="row">
+              <div class="col-lg-12">
+                <div class="star-rating">
+                  <span class="fa fa-star-o" data-rating="1"></span>
+                  <span class="fa fa-star-o" data-rating="2"></span>
+                  <span class="fa fa-star-o" data-rating="3"></span>
+                  <span class="fa fa-star-o" data-rating="4"></span>
+                  <span class="fa fa-star-o" data-rating="5"></span>
+                  <input
+                    type="hidden"
+                    name="whatever1"
+                    class="rating-value"
+                    value="2.56"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
