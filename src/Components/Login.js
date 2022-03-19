d8783db05a4d7bbd903761c3be1d265c4af7ed52
mrudulpatel <mrudulpatel04@gmail.com>
2022-03-19 14:59:38 +0530

    update Login.js

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -21,84 +21,82 @@ function Login(props) {
 
   return !hasAccount ? (
     <div className="loginContainer">
-      <body>
-        <div className="form1">
-          <h1 className="h1">Registration</h1>
-          {/* <label className="details">Canteen Name</label> */}
-          <input
-            type="text"
-            placeholder="Canteen Name"
-            required
-            value={name}
-            onChange={(e) => {
-              setName(e.target.value);
-              localStorage.setItem("name", e.target.value);
-            }}
-          />
-          {/* <label className="details">Contact No.</label> */}
-          <input
-            type="tel"
-            placeholder="Contact No."
-            required
-            value={contact}
-            onChange={(e) => {
-              setContact(e.target.value);
-              localStorage.setItem("contact", e.target.value);
-            }}
-          />
-          {/* <label className="details">Email</label> */}
-          <input
-            type="text"
-            placeholder="Email"
-            required
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <p className="errorMsg">{emailError}</p>
-          {/* <label className="details">Password</label> */}
-          <input
-            placeholder="Password"
-            type="password"
-            required
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <p className="errorMsg">{passwordError}</p>
-          <div className="btn-container">
-            {hasAccount ? ( //true
-              <div className="login_btn">
-                <button className="btn" onClick={handleLogin}>
-                  Login
-                </button>
-                <p>
-                  Don't have an Account?{" "}
-                  <span
-                    style={{ cursor: "pointer" }}
-                    onClick={() => setHasAccount(!hasAccount)}
-                  >
-                    Sign Up
-                  </span>
-                </p>
-              </div>
-            ) : (
-              <div className="login_btn">
-                <button className="btn" onClick={handleSignUp}>
+      <div className="form1">
+        <h1 className="h1">Registration</h1>
+        {/* <label className="details">Canteen Name</label> */}
+        <input
+          type="text"
+          placeholder="Canteen Name"
+          required
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+            localStorage.setItem("name", e.target.value);
+          }}
+        />
+        {/* <label className="details">Contact No.</label> */}
+        <input
+          type="tel"
+          placeholder="Contact No."
+          required
+          value={contact}
+          onChange={(e) => {
+            setContact(e.target.value);
+            localStorage.setItem("contact", e.target.value);
+          }}
+        />
+        {/* <label className="details">Email</label> */}
+        <input
+          type="text"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <p className="errorMsg">{emailError}</p>
+        {/* <label className="details">Password</label> */}
+        <input
+          placeholder="Password"
+          type="password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <p className="errorMsg">{passwordError}</p>
+        <div className="btn-container">
+          {hasAccount ? ( //true
+            <div className="login_btn">
+              <button className="btn" onClick={handleLogin}>
+                Login
+              </button>
+              <p>
+                Don't have an Account?{" "}
+                <span
+                  style={{ cursor: "pointer" }}
+                  onClick={() => setHasAccount(!hasAccount)}
+                >
                   Sign Up
-                </button>
-                <p>
-                  Have an account ?
-                  <span
-                    style={{ cursor: "pointer" }}
-                    onClick={() => setHasAccount(!hasAccount)}
-                  >
-                    Sign In
-                  </span>
-                </p>
-              </div>
-            )}
-          </div>
+                </span>
+              </p>
+            </div>
+          ) : (
+            <div className="login_btn">
+              <button className="btn" onClick={handleSignUp}>
+                Sign Up
+              </button>
+              <p>
+                Have an account ?
+                <span
+                  style={{ cursor: "pointer" }}
+                  onClick={() => setHasAccount(!hasAccount)}
+                >
+                  Sign In
+                </span>
+              </p>
+            </div>
+          )}
         </div>
-      </body>
+      </div>
     </div>
   ) : (
     <div className="loginContainer">
@@ -129,7 +127,7 @@ function Login(props) {
               Login
             </button>
             <p>
-              Don't have an Account?{" "}
+              Don't have an Account?
               <span
                 style={{ cursor: "pointer" }}
                 onClick={() => setHasAccount(!hasAccount)}
